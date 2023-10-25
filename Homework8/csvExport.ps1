b69a7b935685a1212c2a73d520c02b59435b762c
Zack Olson <zachary.olson@mymail.champlain.edu>
2023-10-25 13:32:02 -0400

    Completed first powershell file

---
+++ Homework8/csvExport.ps1
@@ -0,0 +1,3 @@
+﻿Get-Eventlog -list
+$readLog= Read-host -Prompt "Please select a log to review"
+Get-Eventlog -logName $readLog -Newest 3 | Export-csv -NoTypeInformation -Path C:\users\champuser\Desktop\rep.csv
